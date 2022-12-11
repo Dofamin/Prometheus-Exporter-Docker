101fa7d7e21946a618f60bcbd4f5b55815cd14de
Dofamin <los.rones@gmail.com>
2022-12-11 17:52:12 +0300

    fix entrypoint.sh

--- container-image-root/entrypoint.sh
+++ container-image-root/entrypoint.sh
@@ -2,18 +2,6 @@
 set -x
 service ntp start
 
-if [ ! -d /node_exporter ]; then
-    mkdir /node_exporter
-fi
-
-if [ ! -d /prometheus/config ]; then
-    mkdir /prometheus/config
-fi
-
-if [ ! -f /prometheus/config/prometheus.yml ]; then
-    cp ./documentation/examples/prometheus.yml /prometheus/config/prometheus.yml
-fi
-
 if [ "$1" == "" ];
 then
 ./node_exporter \
